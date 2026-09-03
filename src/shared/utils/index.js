31fe0999b101a1f36d1a272eb19f5ae3dfb02725
Osamu2500 <77187399+Osamu2500@users.noreply.github.com>
2026-09-04 00:24:30 +0530

    Fix Utils global assignment issue causing complete extension failure

--- src/shared/utils/index.js
+++ src/shared/utils/index.js
@@ -1,3 +1,12 @@
+import * as core from './modules/core.js';
+import * as debug from './modules/debug.js';
+import * as dom from './modules/dom.js';
+import * as storage from './modules/storage.js';
+import * as ui from './modules/ui.js';
+
+window.YPP = window.YPP || {};
+window.YPP.Utils = { ...core, ...debug, ...dom, ...storage, ...ui };
+
 export * from './modules/core.js';
 export * from './modules/debug.js';
 export * from './modules/dom.js';
